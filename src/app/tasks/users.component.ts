6c4246ece50c460adf0462c5d285f9bdd90c4d73
Marc Hudak <Marc.Hudak@gmail.com>
2018-04-06 08:53:11 -0500

    added tasks

---
+++ src/app/tasks/users.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../user';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  users: User[];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userService.getUsers()
+    .subscribe(users => this.users = users);
+  }
+
+  add(userName: string): void {
+    userName = userName.trim();
+    if (!userName) { return; }
+    this.userService.addUser({ userName } as User)
+      .subscribe(user => {
+        this.users.push(user);
+      });
+  }
+
+  delete(user: User): void {
+    this.users = this.users.filter(h => h !== user);
+    this.userService.deleteUser(user).subscribe();
+  }
+
+}
